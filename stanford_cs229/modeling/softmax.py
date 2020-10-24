Fixed gradient on softmax

stanford_cs229/modeling/softmax.py
@@ -1,8 +1,9 @@
 import numpy as np
 from stanford_cs229.utils.util import *
+from datetime import datetime
 
 class SoftmaxRegression:
-    def __init__(self, lr=0.001, max_iter=1000000, eps=1e-15, verbose=True):
+    def __init__(self, lr=0.001, max_iter=1000000, eps=1e-15, verbose=False):
         self.theta = None
         self.lr = lr
         self.max_iter = max_iter
@@ -22,13 +23,12 @@ class SoftmaxRegression:
             x = X[i]
             y = Y[i]
 
-            probs = np.zeros(x.shape)
+            probs = 0
             for j in range(k):
-                probs += np.exp(np.matmul(self.theta[:,j].T, x))
-            probs = 1/probs
+                probs += np.exp(self.theta[:,j].T.dot(x))
+            probs = 1. / probs
             for l in range(k):
-                result[:, l] = x * ((y == l) - np.exp(self.theta[:,l].T, x) * probs)
-
+                result[:, l] += x * ((y == l) - np.exp(self.theta[:,l].T.dot(x)) * probs)
         return result
 
     def gradient_vec(self, X, Y):
@@ -48,6 +48,28 @@ class SoftmaxRegression:
         return np.argmax(exp, 1)
 
     def train(self, X, Y, k):
+        start = datetime.now()
+        n, d = X.shape
+        self.theta = np.zeros((d, k))
+
+        num_iter = 0
+
+        while True:
+            prev_theta = np.copy(self.theta)
+            self.theta += self.lr * self.gradient(X, Y)
+
+            num_iter += 1
+            update_size = np.linalg.norm(self.theta - prev_theta, ord=1)
+            if self.verbose:
+                print('Iteration {:n}, Update Size {:f}'.format(num_iter, update_size))
+            if num_iter > self.max_iter or np.isnan(update_size) or update_size < self.eps:
+                print('DONE TRAINING')
+                break
+        stop = datetime.now()
+        print('Time: {:n}', stop-start)
+
+    def train_vec(self, X, Y, k):
+        start = datetime.now()
         n, d = X.shape
         self.theta = np.zeros((d, k))
 
@@ -57,6 +79,9 @@ class SoftmaxRegression:
             prev_theta = np.copy(self.theta)
             self.theta += self.lr * self.gradient_vec(X, Y)
 
+            print(self.gradient_vec(X, Y))
+            print(self.gradient(X, Y))
+
             num_iter += 1
             update_size = np.linalg.norm(self.theta - prev_theta, ord=1)
             if self.verbose:
@@ -64,6 +89,8 @@ class SoftmaxRegression:
             if num_iter > self.max_iter or np.isnan(update_size) or update_size < self.eps:
                 print('DONE TRAINING')
                 break
+        stop = datetime.now()
+        print('Time: {:n}', stop-start)
 
 def main():
     X, Y = load_dataset('', add_intercept=True)